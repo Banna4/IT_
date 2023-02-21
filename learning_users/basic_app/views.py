f20e36327311398e105848ebbd0d59d953025167
Akash Singh <akashsinghchouhan990@gmail.com>
2023-02-22 01:02:28 +0530

    again

--- learning_users/basic_app/views.py
+++ learning_users/basic_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from basic_app.forms import UserForm,UserProfileInfoForm
 
 
--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -129,8 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[
+    STATIC_DIR,
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 #MEDIA
 MEDIA_ROOT = MEDIA_DIR
